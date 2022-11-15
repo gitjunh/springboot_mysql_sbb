7f12a50c5fe358e1d8f73c6a83fb7e719f013375
dagachy <41998632+gitjunh@users.noreply.github.com>
2022-11-15 17:02:40 +0900

    Update TestApplicationTests.java
    데이터베이스 접근 테스트 코드 구현

--- src/test/java/com/mysql/test/TestApplicationTests.java
+++ src/test/java/com/mysql/test/TestApplicationTests.java
@@ -1,13 +1,50 @@
 package com.mysql.test;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@AutoWired
+	private QuestionRepository questionRepository;
+	
+// 	@Test
+// 	void testJpa() {
+// 		Question q1 = new Question();
+// 		q1.setSubject("sbb가 무엇인가요?");
+//         q1.setContent("sbb에 대해서 알고 싶습니다.");
+//         q1.setCreateDate(LocalDateTime.now());
+//         this.questionRepository.save(q1);
+        
+//         Question q2 = new Question();
+//         q2.setSubject("스프링부트 질문입니다.");
+//         q2.setContent("id는 자동으로 생성되나요?");
+//         q2.setCreateDate(LocalDatetime.now());
+//         this.questionRepository.save(q2);
+//     }
+    
+//     @Test
+//     void testJpa() {
+//         List<Question> all = this.questionRepository.findAll();
+//         assertEquals(2, all.size());
 
+//         Question q = all.get(0);
+//         assertEquals("sbb가 무엇인가요?", q.getSubject());
+    //     }
+    
+    @Test
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        
+        if(oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
 }
